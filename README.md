a086a565c9ed8068e5aa20eb24a3edc3bd870e38
Jeanette Wiktorsson <leiia@hotmail.com>
2026-02-19 13:24:31 +0100

    GET data from API and show in app

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 Replace this readme with your own information about the project. You can include things like:
 
-- Brief description of the assignment
-- How you approached the task, what tools and techniques you used, and how you planned it
-- If you had more time, what would be next?
-- How to run the project locally
+- This assignment is to build an app like X that fetches data from an API and also posts.
+It will look as close as possible as the provided example.
+- I will start with fetching the latest posts, defining the order.
+- After that I will setup the POST request.
+- Lastly I will work on the styling and CSS.
+
 
 ## View it live
 Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
+//hämta useEffect from React
+import { useEffect, useState } from "react";
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>
-}
+  const [thoughts, setThoughts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  //hämta posts från community API, omvandla svaret till data och spara i state
+  useEffect(() => {
+    fetch("https://happy-thoughts-api-4ful.onrender.com/thoughts")
+      .then((res) => {
+        if (!res.ok) throw new Error("Could not get any happy thoughts :(");
+        return res.json();
+      })
+      .then((data) => setThoughts(data))
+      .catch((err) => setError(err.message))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error}</p>
+
+  return (<div>
+    {thoughts.map((thought) => (
+      <p key={thought._id}>{thought.message}</p>
+    ))}</div>
+  );
+};
